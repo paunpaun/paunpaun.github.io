b3a9c3e2d6307953d3615610c935571cf5c139e3
paunpaun <abc795651@gmail.com>
2024-09-26 16:13:51 +0200

    committt

--- script/script.js
+++ script/script.js
@@ -4,57 +4,45 @@ function switch_section(section) {
     const buttons = document.querySelectorAll('#sections button');
     
     buttons.forEach(button => button.classList.remove('active'));
-    
     document.getElementById(`${section}-btn`).classList.add('active');
-
 
     if (section === 'home') {
         main_content.innerHTML = `
             <div id="first-line">
-                <img src="/assets/pictures/pfp.png" alt="" style="margin-right: 1em;">
+                <img src="/assets/pictures/pfp.png" style="width: 10em; margin: 1em;">
                 Paun
             </div>
-            <p>home<p>
-        `;
+            <p>paunpaunpaunpaunpaunpaunpaunpaunpaunpaunpaunpaun<p>
+`;
     } else if (section === 'about') {
         main_content.innerHTML = `
             <h2>About Me</h2>
+            <p>paunpaunpaunpaunpaunpaunpaun<p>
+            <p>paunpaunpaunpaunpaunpaunpaunpaun<p>
+            <br>
+            <p>paunpaunpaunpaunpaunpaunpaunpaunpaunpaunpaun<p>
+            <p>paunpaunpaunpaunpaunpaunpaunpaun<p>
         `;
     } else if (section === 'projects') {
         main_content.innerHTML = `
             <h2>My Projects</h2>
             <h3>Project x</h3>
             <p>project does x</p>
-            <div class="scrollable-div">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-                <img src="/assets/pictures/steam2.webp">
-            </div>              
-            <a href="https://github.com/paunpaun/projectxr">
-            github link
-            </a>            
+            <div class="projects-div">
+                <img src="/assets/pictures/github4.webp" style="width: 10em;">
+                <img src="/assets/pictures/github4.webp" style="width: 10em;">
+                <img src="/assets/pictures/github4.webp" style="width: 10em;">
+            </div>                     
         `;
     } else if (section === 'contacts') {
         main_content.innerHTML = `
             <h2>Contact Me</h2>
-            <ul>
-                <a href="https://github.com/paunpaun" target="_blank">GitHub</a></li>
-            </ul>
+            <div>
+                <img src="/assets/pictures/github4.webp" style="width: 5em; margin: 1em">
+                <a href="https://github.com/paunpaun" target="_blank" >
+                GitHub
+                </a>
+            </div>
         `;
     }
 }
